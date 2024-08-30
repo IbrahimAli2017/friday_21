4c2613cacefda2cc65f059b6522580aeb6dc8fc8
IbrahimAli2017 <tech.solutions.im@gmail.com>
2024-08-30 11:38:11 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+class Node{
+public:
+    int value = 0;
+    Node* left = NULL;
+    Node* right = NULL;
+};
+class BST{
+Node* root = NULL;
+public:
+    void insert(int value){
+        Node* n = new Node();
+    }
+
+};
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
